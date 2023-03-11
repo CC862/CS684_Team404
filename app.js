55b1ae320c3bfbe2feea0c5738af113fa7e4e779
Akshay <akshaymkgm98@gmail.com>
2023-03-11 13:18:50 -0500

    API connection

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 const flash = require('connect-flash');
 const session = require('express-session');
+const axios = require("axios");
 
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-
 var home = require("./routes/home");
 var data = require("./routes/404");
 var dashboardRouter = require("./routes/dashboard");
 var registerRouter = require("./routes/signup");
 var logoutRouter = require("./routes/signout");
 var loginRouter = require("./routes/signin");
+var newsRouter = require("./routes/news");
 
 var app = express();
 
@@ -41,6 +42,11 @@ app.use("/dashboard", dashboardRouter);
 app.use("/signup", registerRouter);
 app.use("/signout", logoutRouter);
 app.use("/signin", loginRouter);
+app.use("/news", newsRouter);
+
+
+
+
 
 var listener = app.listen(8080, function () {
   console.log("Listening on port " + listener.address().port);

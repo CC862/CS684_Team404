ffe18b84e999200d1e4882c465d65822f5d92bc7
joshua <joshuasteinerman@gmail.com>
2024-01-14 18:04:55 -0500

    chnaged db

--- db/db.js
+++ db/db.js
@@ -3,9 +3,9 @@ var mysql2 = require('mysql2/promise');
 
 var pool  = mysql2.createPool({
   connectionLimit : 10,
-  host            : 'us-east.connect.psdb.cloud',
-  user            : 'evf9sx5vuplnvljp2i65',
-  password        : 'pscale_pw_pyiXZG4J2b6sJF5ad17ExOnjlPkVU8J9LIuaoMjPisr',
+  host            : 'aws.connect.psdb.cloud',
+  user            : 'y8sja6rykpmxv5ju8qbv',
+  password        : 'pscale_pw_Qrk6FfaTdDrCsNFNvDh3ftG6N4y2Jnqc37kHHJQ00BV',
   database        : 'cs_631_project',
   ssl : {
       rejectUnauthorized: true
